46806578d9e5b4d2e593b81559ea63a6da3f656d
ao1 <ao1@ra.rockwell.com>
2016-05-25 16:47:29 +0800

    implement pow.s

--- pow32.s
+++ pow32.s
@@ -17,7 +17,7 @@ _start:
     pushl $2 #push first argument, the base
     call power #push the address of next instruction to stack
                #set %eip to the address of power function.
-    addl $16, %esp #pop the 2 arguments from stack.
+    addl $8, %esp #pop the 2 arguments from stack.
 
     # save the first answer, before calling power again
     pushl %eax # 1 byte instruction encoded as pushl %eax in 32 bits
@@ -28,7 +28,7 @@ _start:
     pushl $5 #push first argument, the base
     call power #push the address of next instruction to stack
                #set %eip to the address of power function.
-    addl $16, %esp #pop the 2 arguments from stack.
+    addl $8, %esp #pop the 2 arguments from stack.
 
     # save the second answer to ebx
     popl %ebx # same as popl %ebx
@@ -42,5 +42,25 @@ _start:
 .type power, @function
 
 power:
-    movl $2, %eax
+    pushl %ebp   # save old base pointer
+    movl %esp, %ebp # save the stack pointer in the base pointer
+    subl $4, %esp # get room for local storage
+
+    movl 8(%ebp), %ebx #put first argument in %ebx
+    movl 12(%ebp), %ecx #put second argument in %ecx
+
+    movl %ebx, -4(%ebp) #store current result
+power_loop_start:
+    cmpl $1, %ecx  #if the power is 1, we are done
+    je end_power
+    movl -4(%ebp),%eax #move the current result into %eax
+    imull %ebx, %eax #multiply the current result by base number.
+    movl %eax, -4(%ebp) #store the current result
+    decl %ecx #decrement the power
+    jmp power_loop_start # next iteration
+
+end_power:
+    movl -4(%ebp),%eax # return value goes in %eax
+    movl %ebp, %esp #restore stack pointer
+    popl %ebp   #restore base pointer    
     ret			# pop a value from stack and set %eip to the value
